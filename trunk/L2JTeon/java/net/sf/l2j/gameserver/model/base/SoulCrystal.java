4dee67ac2d7a069ba31ed3bf5501488a62a0be52
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-09-03 00:53:27 +0000

    '''CORE:''' Config's for Soul Crystal.

--- trunk/L2JTeon/java/net/sf/l2j/gameserver/model/base/SoulCrystal.java
+++ trunk/L2JTeon/java/net/sf/l2j/gameserver/model/base/SoulCrystal.java
@@ -18,6 +18,7 @@
  */
 package net.sf.l2j.gameserver.model.base;
 
+import net.sf.l2j.Config;
 /**
  * $ Rewrite 06.12.06 - Yesod
  */
@@ -42,8 +43,8 @@ public class SoulCrystal
      */
     public static final int[] SoulCrystalTable = { 4629, 4630, 4631, 4632, 4633, 4634, 4635, 4636, 4637, 4638, 4639, 5577, 5580, 5908, 4640, 4641, 4642, 4643, 4644, 4645, 4646, 4647, 4648, 4649, 4650, 5578, 5581, 5911, 4651, 4652, 4653, 4654, 4655, 4656, 4657, 4658, 4659, 4660, 4661, 5579, 5582, 5914 };
     public static final int MAX_CRYSTALS_LEVEL = 13;
-    public static final int BREAK_CHANCE = 10;
-    public static final int LEVEL_CHANCE = 32;
+    public static final int BREAK_CHANCE = Config.SOUL_CRYSTAL_BREAK_CHANCE;
+    public static final int LEVEL_CHANCE = Config.SOUL_CRYSTAL_LEVEL_CHANCE;
     public static final int RED_BROKEN_CRYSTAL = 4662;
     public static final int GRN_BROKEN_CYRSTAL = 4663;
     public static final int BLU_BROKEN_CRYSTAL = 4664;
