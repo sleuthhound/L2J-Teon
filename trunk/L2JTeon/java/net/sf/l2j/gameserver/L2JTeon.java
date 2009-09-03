8561cb05d2ff067240a479eb787ea7586610c5b2
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-09-03 22:18:21 +0000

    '''INFO:''' Removed Game and DataPack version the initialization the GameServer, <br> 			L2J Teon Info added Server and DataPack Version.

--- trunk/L2JTeon/java/net/sf/l2j/gameserver/L2JTeon.java
+++ trunk/L2JTeon/java/net/sf/l2j/gameserver/L2JTeon.java
@@ -2,24 +2,26 @@ package net.sf.l2j.gameserver;
 
 import java.util.logging.Logger;
 
-public class L2JTeon {
-
+public class L2JTeon 
+{
 	private static final Logger _log = Logger.getLogger(GameServer.class.getName());
 	/**
 	 *  L2JTeon Info
 	 */
 	public static void L2JTeon()
 	{
-	_log.info("-----------------------------------------------------");
-	_log.info("#      # # #   # # #   # # #  # # #    ####   #   #  ");
-	_log.info("#          #     #       #    #       #    #  ##  #  ");
-	_log.info("#        #       #       #    # # #   #    #  # # #  ");
-	_log.info("#      #      #  #       #    #       #    #  #  ##  ");
-	_log.info("# # #  # # #  ####       #    # # #    ####   #   #  ");
-	_log.info("-----------------------------------------------------");
-	_log.info("                  Project Interlude                  ");
-	_log.info("                      Game Server                    ");
-	_log.info("             Developers: Maxi56, Meyknho             ");
-	_log.info("-----------------------------------------------------");
+		_log.info("-----------------------------------------------------");
+		_log.info("           Developers: Maxi56, Meyknho               ");
+		_log.info("-----------------------------------------------------");
+		_log.info("#      # # #   # # #   # # #  # # #    ####   #   #  ");
+		_log.info("#          #     #       #    #       #    #  ##  #  ");
+		_log.info("#        #       #       #    # # #   #    #  # # #  ");
+		_log.info("#      #      #  #       #    #       #    #  #  ##  ");
+		_log.info("# # #  # # #  ####       #    # # #    ####   #   #  ");
+		_log.info("-----------------------------------------------------");
+		_log.info("   L2J Teon Core Version: "+Config.SERVER_VERSION     );
+		_log.info(" L2J Teon DataPack Version: "+Config.DATAPACK_VERSION );
+		_log.info("               Copyright 2009-2010                   ");
+		_log.info("-----------------------------------------------------");
 	}
 }
