a0d21b885fee7aae8d9a98416c72ca90422d0927
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-09-08 19:55:23 +0000

    '''PART - 1''' /*  * This program is free software: you can redistribute it and/or modify it under  * the terms of the GNU General Public License as published by the Free Software  * Foundation, either version 3 of the License, or (at your option) any later  * version.  *  * This program is distributed in the hope that it will be useful, but WITHOUT  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more  * details.  *  * You should have received a copy of the GNU General Public License along with  * this program. If not, see <http://www.gnu.org/licenses/>.  */

--- trunk/L2JTeon/java/net/sf/l2j/gameserver/L2JTeon.java
+++ trunk/L2JTeon/java/net/sf/l2j/gameserver/L2JTeon.java
@@ -1,3 +1,17 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.l2j.gameserver;
 
 import java.util.logging.Logger;
@@ -23,7 +37,7 @@ public class L2JTeon
 		_log.info("--------------------------------------------------------");
 		_log.info("    L2J Teon Core Version: "+Config.SERVER_VERSION       );
 		_log.info("  L2J Teon DataPack Version: "+Config.DATAPACK_VERSION   );
-		_log.info("                 Copyright © 2009-2010                  ");
+		_log.info("                   Copyright 2009-2010                  ");
 		_log.info("--------------------------------------------------------");	
 	}
 }
