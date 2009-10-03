d86c6c586d797d362bc0c3b839fcbac543cc44b9
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-10-03 06:15:24 +0000

    TeonPacketHandler Removed Part 1/2.

--- trunk/L2JTeon/java/net/sf/l2j/gameserver/clientpackets/L2GameClientPacket.java
+++ trunk/L2JTeon/java/net/sf/l2j/gameserver/clientpackets/L2GameClientPacket.java
@@ -31,7 +31,6 @@ public abstract class L2GameClientPacket extends ReceivablePacket<L2GameClient>
 {
     private static final Logger _log = Logger.getLogger(L2GameClientPacket.class.getName());
 
-    protected int _opcode = 0;
     @Override
     protected boolean read()
     {
