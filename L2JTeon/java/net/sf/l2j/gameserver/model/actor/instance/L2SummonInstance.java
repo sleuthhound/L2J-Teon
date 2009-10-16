8202a2f5622ff3360a00232c45aa6f2387a326ae
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-10-16 11:46:15 +0000

    '''CORE''' Retail Olympiad !!! Working now in L2J Teon.

--- L2JTeon/java/net/sf/l2j/gameserver/model/actor/instance/L2SummonInstance.java
+++ L2JTeon/java/net/sf/l2j/gameserver/model/actor/instance/L2SummonInstance.java
@@ -349,6 +349,14 @@ public class L2SummonInstance extends L2Summon
     	{
     		if (pcrit || mcrit)
     			getOwner().sendPacket(new SystemMessage(SystemMessageId.CRITICAL_HIT_BY_SUMMONED_MOB));
+			
+    		if (getOwner().isInOlympiadMode() &&
+    				target instanceof L2PcInstance &&
+    				((L2PcInstance)target).isInOlympiadMode() &&
+    				((L2PcInstance)target).getOlympiadGameId() == getOwner().getOlympiadGameId())
+    		{
+    			getOwner().dmgDealt += damage;
+    		}
 
     		SystemMessage sm = new SystemMessage(SystemMessageId.SUMMON_GAVE_DAMAGE_S1);
     		sm.addNumber(damage);
