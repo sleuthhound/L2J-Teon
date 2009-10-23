4aabaf5a3eb20ba168b72702bc0d1d6cd2ba00f0
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-10-23 23:24:45 +0000

    MOVED ADMIN COMMAND HANDLERS AND ITEM HANDLERS TO CORE!!!
    Facilitate updates ( Replace the '''l2jteon.jar''' == update rev. )!!!

--- trunk/L2JTeon/java/net/sf/l2j/gameserver/handler/ItemHandler.java
+++ trunk/L2JTeon/java/net/sf/l2j/gameserver/handler/ItemHandler.java
@@ -14,11 +14,19 @@
  */
 package net.sf.l2j.gameserver.handler;
 
+import java.util.logging.Logger;
 import java.util.Map;
 import java.util.TreeMap;
 
+import net.sf.l2j.gameserver.handler.itemhandlers.*;
+
+/**
+ * @author  Maxi
+ */
 public class ItemHandler
 {
+	private static Logger _log = Logger.getLogger(ItemHandler.class.getName());
+
 	private Map<Integer, IItemHandler> _datatable;
 
 	public static ItemHandler getInstance()
@@ -34,6 +42,43 @@ public class ItemHandler
 	private ItemHandler()
 	{
 		_datatable = new TreeMap<Integer, IItemHandler>();
+		registerItemHandler(new ScrollOfEscape());
+		registerItemHandler(new ScrollOfResurrection());
+		registerItemHandler(new SoulShots());
+		registerItemHandler(new SpiritShot());
+		registerItemHandler(new BlessedSpiritShot());
+		registerItemHandler(new BeastSoulShot());
+		registerItemHandler(new BeastSpiritShot());
+		registerItemHandler(new ChestKey());
+		registerItemHandler(new DungeonKeys());
+		registerItemHandler(new Maps());
+		registerItemHandler(new Potions());
+		registerItemHandler(new Recipes());
+		registerItemHandler(new RollingDice());
+		registerItemHandler(new MysteryPotion());
+		registerItemHandler(new EnchantScrolls());
+		registerItemHandler(new EnergyStone());
+		registerItemHandler(new Book());
+		registerItemHandler(new Remedy());
+		registerItemHandler(new Scrolls());
+		registerItemHandler(new CrystalCarol());
+		registerItemHandler(new DonatorItems());
+		registerItemHandler(new SoulCrystals());
+		registerItemHandler(new SevenSignsRecord());
+		registerItemHandler(new CharChangePotions());
+		registerItemHandler(new Firework());
+		registerItemHandler(new Seed());
+		registerItemHandler(new Harvester());
+		registerItemHandler(new MercTicket());
+		registerItemHandler(new FishShots());
+		registerItemHandler(new JackpotSeed());
+		registerItemHandler(new ExtractableItems());
+		registerItemHandler(new SpecialXMas());
+		registerItemHandler(new SummonItems());
+		registerItemHandler(new BeastSpice());
+		registerItemHandler(new PrimevalPotions());
+		registerItemHandler(new ScrollsValakas());
+		_log.config("Loaded " + ItemHandler.getInstance().size() + " ItemHandlers");
 	}
 
 	public void registerItemHandler(IItemHandler handler)
