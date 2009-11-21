d84be9cd57286f5a10709e27be56fb78814437c8
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-11-21 14:20:20 +0000

    Fix (PHX) Exploits for WareHouse.

--- trunk/L2JTeon/java/net/sf/l2j/gameserver/network/clientpackets/SendWareHouseDepositList.java
+++ trunk/L2JTeon/java/net/sf/l2j/gameserver/network/clientpackets/SendWareHouseDepositList.java
@@ -95,11 +95,17 @@ public final class SendWareHouseDepositList extends L2GameClientPacket
 		L2WarehouseInstance target = null;
 		if (player.getActiveEnchantItem() != null && target instanceof L2WarehouseInstance)
 		{
-			player.sendPacket(ActionFailed.STATIC_PACKET);
+            player.setAccessLevel(-100); 
+            player.setAccountAccesslevel(-100); 
+            Util.handleIllegalPlayerAction(player, "Player " + player.getName() + " tried to use enchant Exploit!", IllegalPlayerAction.PUNISH_KICKBAN); 
+            return; 
 		}
 		if (player.getActiveEnchantItem() != null)
 		{
-			player.sendPacket(ActionFailed.STATIC_PACKET);
+            player.setAccessLevel(-100); 
+            player.setAccountAccesslevel(-100); 
+            Util.handleIllegalPlayerAction(player, "Player " + player.getName() + " tried to use enchant Exploit!", IllegalPlayerAction.PUNISH_KICKBAN); 
+            return; 
 		}
 		// Alt game - Karma punishment
 		if (!Config.ALT_GAME_KARMA_PLAYER_CAN_USE_WAREHOUSE && (player.getKarma() > 0))
