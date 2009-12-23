b99ec16186c222c5463715277e52e9bd84f314bd
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-12-23 10:26:53 +0000

    Fixed auto announcements - FINAL PART.

--- trunk/L2JTeon-dp/sql/autoanounce.sql
+++ trunk/L2JTeon-dp/sql/autoanounce.sql
@@ -1,6 +1,9 @@
+-- ----------------------------
+-- Table structure for auto_announcements
+-- ----------------------------
 CREATE TABLE `auto_announcements` (
-`id` int(11) NOT NULL auto_increment,
-`announcement` varchar(255) collate latin1_general_ci NOT NULL,
-`delay` int(11) NOT NULL,
-PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `announcement` varchar(255) COLLATE latin1_general_ci NOT NULL DEFAULT '',
+  `delay` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
