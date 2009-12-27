1768b36b367c42ff1f8e8e2d388a9d898e79dd81
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2009-12-27 15:50:32 +0000

    This will add a new setting to skill effects that allows the skill effects icon to be ignored so for example if you have 2 skill effects for 1 skill like Invocation, which are Over Time effects, you can make it so one of the effects icon request is ignored.     Thanks L2j Server Team!!!

--- L2JTeon/java/net/sf/l2j/gameserver/model/L2Effect.java
+++ L2JTeon/java/net/sf/l2j/gameserver/model/L2Effect.java
@@ -123,6 +123,8 @@ public abstract class L2Effect
 	private int _count;
 	// abnormal effect mask
 	private int _abnormalEffect;
+    // show icon
+    private boolean _icon;
 	public boolean preventExitUpdate;
 
 	public final class EffectTask implements Runnable
@@ -179,6 +181,7 @@ public abstract class L2Effect
 		_stackOrder = template.stackOrder;
 		_periodStartTicks = GameTimeController.getGameTicks();
 		_periodfirsttime = 0;
+        _icon = template.icon;
 		scheduleEffect();
 	}
 
@@ -213,6 +216,11 @@ public abstract class L2Effect
 			_currentFuture = ThreadPoolManager.getInstance().scheduleEffect(_currentTask, duration * 1000);
 		}
 	}
+	
+    public boolean getShowIcon()
+    {
+        return _icon;
+    }
 
 	public int getPeriod()
 	{
