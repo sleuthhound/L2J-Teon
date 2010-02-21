ad27ee67b13ad9be1396645618f13cf5d7adad82
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-02-21 02:23:17 +0000

    Possible fix for skills Mass Recharge and Recharge and other skills with effect ManaHeal. Possivel correcao dos skills Mass Recharge and Recharge e outros skills com efeito ManaHeal.

--- L2JTeon/java/net/sf/l2j/gameserver/handler/skillhandlers/ManaHeal.java
+++ L2JTeon/java/net/sf/l2j/gameserver/handler/skillhandlers/ManaHeal.java
@@ -1,16 +1,20 @@
 /*
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * http://www.gnu.org/copyleft/gpl.html
  */
 package net.sf.l2j.gameserver.handler.skillhandlers;
 
@@ -24,79 +28,62 @@ import net.sf.l2j.gameserver.network.SystemMessageId;
 import net.sf.l2j.gameserver.network.serverpackets.StatusUpdate;
 import net.sf.l2j.gameserver.network.serverpackets.SystemMessage;
 import net.sf.l2j.gameserver.skills.Stats;
-
 /**
- * Mana Heal Skills Handler (Recharge and etc.) Date: 2007/08/31
+ * This class ...
+ *
+ * @version $Revision: 1.1.2.2.2.1 $ $Date: 2005/03/02 15:38:36 $
  */
+
 public class ManaHeal implements ISkillHandler
 {
-	/*
-	 * (non-Javadoc)
+	//private static Logger _log = Logger.getLogger(ManaHeal.class.getName());
+
+	/* (non-Javadoc)
 	 * @see net.sf.l2j.gameserver.handler.IItemHandler#useItem(net.sf.l2j.gameserver.model.L2PcInstance, net.sf.l2j.gameserver.model.L2ItemInstance)
 	 */
-	private static final SkillType[] SKILL_IDS = { SkillType.MANAHEAL, SkillType.MANARECHARGE, SkillType.MANAHEAL_PERCENT };
+	private static final SkillType[] SKILL_IDS = {SkillType.MANAHEAL, SkillType.MANARECHARGE, SkillType.MANAHEAL_PERCENT};
 
-	/*
-	 * (non-Javadoc)
+	/* (non-Javadoc)
 	 * @see net.sf.l2j.gameserver.handler.IItemHandler#useItem(net.sf.l2j.gameserver.model.L2PcInstance, net.sf.l2j.gameserver.model.L2ItemInstance)
 	 */
 	public void useSkill(L2Character actChar, L2Skill skill, L2Object[] targets)
 	{
-		SystemMessage sm;
-		L2Character target = null;
-		L2PcInstance targetInstance = null;
-		int classId = 0;
-		int skillId = 0;
-		for (L2Object target2 : targets) {
-			target = (L2Character) target2;
-			if (target instanceof L2PcInstance)
-			{
-				targetInstance = (L2PcInstance) target;
-				classId = targetInstance.getClassId().getId();
-				skillId = skill.getId();
-				// skill "recharge" cant be used on player-classes that own it,
-				// self too (in Interlude)
-				if ((skillId == 1013) && ((classId == 29) || (classId == 30) || (classId == 42) || (classId == 43)))
-				{
-					if (actChar instanceof L2PcInstance)
-					{
-						sm = new SystemMessage(SystemMessageId.TARGET_IS_INCORRECT);
-						actChar.sendPacket(sm);
-					}
-					return;
-				}
-			}
-			// calculate MP heal power
-			double mp = skill.getPower();
-			if (skill.getSkillType() == SkillType.MANAHEAL_PERCENT)
-			{
-				mp = target.getMaxMp() * mp / 100.0;
-			}
-			else
-			{
-				mp = skill.getSkillType() == SkillType.MANARECHARGE ? target.calcStat(Stats.RECHARGE_MP_RATE, mp, null, null) : mp;
-			}
-			// heal MP
-			target.setLastHealAmount((int) mp);
-			target.setCurrentMp(mp + target.getCurrentMp());
+        L2Character target = null;
+
+        for (L2Object target2 : targets) {
+            target = (L2Character)target2;
+            double mp = skill.getPower();
+            if (skill.getSkillType() == SkillType.MANAHEAL_PERCENT){
+            	//double mp = skill.getPower();
+             mp = target.getMaxMp() * mp / 100.0;
+            }
+            else {
+             mp = (skill.getSkillType() == SkillType.MANARECHARGE) ? target.calcStat(Stats.RECHARGE_MP_RATE,mp, null, null) : mp;
+            }
+			//int cLev = activeChar.getLevel();
+			//hp += skill.getPower()/*+(Math.sqrt(cLev)*cLev)+cLev*/;
+            target.setLastHealAmount((int)mp);
+			target.setCurrentMp(mp+target.getCurrentMp());
 			StatusUpdate sump = new StatusUpdate(target.getObjectId());
-			sump.addAttribute(StatusUpdate.CUR_MP, (int) target.getCurrentMp());
+			sump.addAttribute(StatusUpdate.CUR_MP, (int)target.getCurrentMp());
 			target.sendPacket(sump);
-			// creating system messege about restore MP
-			if (actChar instanceof L2PcInstance)
-			{
-				sm = new SystemMessage(SystemMessageId.S2_MP_RESTORED_BY_S1);
-				sm.addString(actChar.getName());
-			}
-			else
-			{
-				sm = new SystemMessage(SystemMessageId.S1_MP_RESTORED);
-			}
-			// send message
-			sm.addNumber((int) mp);
-			target.sendPacket(sm);
+
+            if (actChar instanceof L2PcInstance && actChar != target)
+            {
+                SystemMessage sm = new SystemMessage(SystemMessageId.S2_MP_RESTORED_BY_S1);
+                sm.addString(actChar.getName());
+                sm.addNumber((int)mp);
+                target.sendPacket(sm);
+            }
+            else
+            {
+                SystemMessage sm = new SystemMessage(SystemMessageId.S1_MP_RESTORED);
+                sm.addNumber((int)mp);
+                target.sendPacket(sm);
+            }
 		}
 	}
+
 
 	public SkillType[] getSkillIds()
 	{
