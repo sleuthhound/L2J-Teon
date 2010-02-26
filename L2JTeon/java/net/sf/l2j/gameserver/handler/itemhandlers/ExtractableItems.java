6b68af5ffaa9afc25cac21cd76d101bad3c5595b
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-02-26 11:47:24 +0000

    Augmentation: Fixed glow and skills / Fixado glow e skills. SiegeFlag: Fixed in Sieges / Fixado nas Sieges! ExtractableItems: Player not more duplicate the items / Jogadores nao consegue mais duplicar os items.
    '''Thanks Contributions:''' Dannrz.

--- L2JTeon/java/net/sf/l2j/gameserver/handler/itemhandlers/ExtractableItems.java
+++ L2JTeon/java/net/sf/l2j/gameserver/handler/itemhandlers/ExtractableItems.java
@@ -14,11 +14,6 @@
  */
 package net.sf.l2j.gameserver.handler.itemhandlers;
 
-import java.util.Iterator;
-import java.util.Vector;
-import java.util.concurrent.Future;
-
-import net.sf.l2j.gameserver.ThreadPoolManager;
 import net.sf.l2j.gameserver.datatables.ExtractableItemsData;
 import net.sf.l2j.gameserver.datatables.ItemTable;
 import net.sf.l2j.gameserver.handler.IItemHandler;
@@ -29,21 +24,11 @@ import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2PlayableInstance;
 import net.sf.l2j.gameserver.model.item.PcInventory;
 import net.sf.l2j.gameserver.network.SystemMessageId;
-import net.sf.l2j.gameserver.network.clientpackets.Say2;
-import net.sf.l2j.gameserver.network.serverpackets.CreatureSay;
-import net.sf.l2j.gameserver.network.serverpackets.ItemList;
-import net.sf.l2j.gameserver.network.serverpackets.StatusUpdate;
 import net.sf.l2j.gameserver.network.serverpackets.SystemMessage;
 import net.sf.l2j.util.Rnd;
 
-/**
- * @author FBIagent 11/12/2006
- */
 public class ExtractableItems implements IItemHandler
 {
-	public static Vector<DelayedOpen> vectDelayedOpen = new Vector<DelayedOpen>();
-	@SuppressWarnings("unchecked")
-	public Future _actionTask;
 
 	public void useItem(L2PlayableInstance playable, L2ItemInstance item)
 	{
@@ -53,123 +38,60 @@ public class ExtractableItems implements IItemHandler
 		L2ExtractableItem exitem = ExtractableItemsData.getInstance().getExtractableItem(item.getItemId());
 		if (exitem == null)
 			return;
-		if (isPlayerNotInVector(activeChar))
+
+		int itemID = item.getItemId();
+		int createItemID = 0, createAmount = 0, rndNum = Rnd.get(100), chanceFrom = 0;
+
+		for (L2ExtractableProductItem expi : exitem.getProductItemsArray())
 		{
-			activeChar.sitDown();
-			DelayedOpen task = new DelayedOpen(activeChar, item);
-			vectDelayedOpen.add(task);
-			_actionTask = ThreadPoolManager.getInstance().scheduleEffectAtFixedRate(task, 0, 1000);
+			int chance = expi.getChance();
+			if (rndNum >= chanceFrom && rndNum <= chance + chanceFrom)
+			{
+				createItemID = expi.getId();
+				createAmount = expi.getAmmount();
+				break;
+			}
+			chanceFrom += chance;
+		}
+		if (createItemID == 0)
+		{
+			activeChar.sendMessage("Nothing happend.");
+			return;
+		}
+		PcInventory inv = activeChar.getInventory();
+		if (createItemID > 0)
+		{
+			if (ItemTable.getInstance().createDummyItem(createItemID).isStackable())
+				inv.addItem("Extract", createItemID, createAmount, activeChar, null);
+			else
+			{
+				for (int i = 0; i < createAmount; i++)
+					inv.addItem("Extract", createItemID, 1, activeChar, item);
+			}
+			SystemMessage sm;
+			if (createAmount > 1)
+			{
+				sm = new SystemMessage(SystemMessageId.EARNED_S2_S1_S);
+				sm.addItemName(createItemID);
+				sm.addNumber(createAmount);
+			}
+			else
+			{
+				sm = new SystemMessage(SystemMessageId.EARNED_ITEM);
+				sm.addItemName(createItemID);
+			}
+			activeChar.sendPacket(sm);
 		}
 		else
-			activeChar.sendMessage("A spot of extraction is already in hand for this character");
+		{
+			activeChar.sendMessage("Item failed to open");
+
+		}
+		activeChar.destroyItemByItemId("Extract", itemID, 1, activeChar.getTarget(), true);
 	}
 
 	public int[] getItemIds()
 	{
 		return ExtractableItemsData.getInstance().itemIDs();
-	}
-
-	private boolean isPlayerNotInVector(L2PcInstance player)
-	{
-		Iterator<DelayedOpen> itDelayedOpen = vectDelayedOpen.iterator();
-		while (itDelayedOpen.hasNext())
-		{
-			DelayedOpen task = itDelayedOpen.next();
-			if (player == task.getActiveChar())
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private class DelayedOpen implements Runnable
-	{
-		private L2PcInstance activeChar;
-		private L2ItemInstance item;
-		private int max;
-		private int current = 0;
-
-		public DelayedOpen(L2PcInstance p, L2ItemInstance i)
-		{
-			activeChar = p;
-			item = i;
-			max = activeChar.getInventory().getInventoryItemCount(item.getItemId(), -1);
-		}
-
-		public L2PcInstance getActiveChar()
-		{
-			return activeChar;
-		}
-
-		public void run()
-		{
-			current++;
-			if (current > max)
-			{
-				_actionTask.cancel(true);
-				_actionTask = null;
-				ExtractableItems.vectDelayedOpen.remove(this);
-				return;
-			}
-			int itemID = item.getItemId();
-			L2ExtractableItem exitem = ExtractableItemsData.getInstance().getExtractableItem(itemID);
-			int createItemID = 0, createAmount = 0, rndNum = Rnd.get(100), chanceFrom = 0;
-			// calculate extraction
-			for (L2ExtractableProductItem expi : exitem.getProductItemsArray())
-			{
-				int chance = expi.getChance();
-				if (rndNum >= chanceFrom && rndNum <= chance + chanceFrom)
-				{
-					createItemID = expi.getId();
-					createAmount = expi.getAmmount();
-					break;
-				}
-				chanceFrom += chance;
-			}
-			if (createItemID == 0)
-			{
-				activeChar.sendMessage("Nothing happend.");
-				return;
-			}
-			PcInventory inv = activeChar.getInventory();
-			if (createItemID > 0)
-			{
-				if (ItemTable.getInstance().createDummyItem(createItemID).isStackable())
-					inv.addItem("Extract", createItemID, createAmount, activeChar, null);
-				else
-				{
-					for (int i = 0; i < createAmount; i++)
-						inv.addItem("Extract", createItemID, 1, activeChar, item);
-				}
-				SystemMessage sm;
-				if (createAmount > 1)
-				{
-					sm = new SystemMessage(SystemMessageId.EARNED_S2_S1_S);
-					sm.addItemName(createItemID);
-					sm.addNumber(createAmount);
-				}
-				else
-				{
-					sm = new SystemMessage(SystemMessageId.EARNED_ITEM);
-					sm.addItemName(createItemID);
-				}
-				activeChar.sendPacket(sm);
-			}
-			else
-			{
-				activeChar.sendMessage("Item failed to open"); // TODO: Put a
-				// more proper
-				// message here.
-			}
-			activeChar.destroyItemByItemId("Extract", itemID, 1, activeChar.getTarget(), true);
-			activeChar.sendPacket(new ItemList(activeChar, false));
-			StatusUpdate su = new StatusUpdate(activeChar.getObjectId());
-			su.addAttribute(StatusUpdate.CUR_LOAD, activeChar.getCurrentLoad());
-			activeChar.sendPacket(su);
-			String _text = current + "/" + max;
-			CreatureSay cs = new CreatureSay(activeChar.getObjectId(), Say2.TELL, activeChar.getName(), _text);
-			activeChar.sendPacket(cs);
-		}
 	}
 }
