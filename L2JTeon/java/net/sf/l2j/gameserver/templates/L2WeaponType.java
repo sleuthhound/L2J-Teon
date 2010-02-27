e57258e9807f136b6b7944347f0ea86d597b4ec7
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-02-27 14:14:21 +0000

    Removed Cross Bow not used in version Interlude. Removido Cross Bow nao usado em versao Interlude.

--- L2JTeon/java/net/sf/l2j/gameserver/templates/L2WeaponType.java
+++ L2JTeon/java/net/sf/l2j/gameserver/templates/L2WeaponType.java
@@ -21,12 +21,19 @@ package net.sf.l2j.gameserver.templates;
 public enum L2WeaponType
 {
 	NONE(1, "Shield"), // Shields!!!
-	SWORD(2, "Sword"), BLUNT(3, "Blunt"), DAGGER(4, "Dagger"), BOW(5, "Bow"), POLE(6, "Pole"), ETC(7, "Etc"), FIST(8, "Fist"), DUAL(9, "Dual Sword"), DUALFIST(10, "Dual Fist"), BIGSWORD(11, "Big Sword"), // Two
-	// Handed
-	// Swords
-	PET(12, "Pet"), ROD(13, "Rod"), BIGBLUNT(14, "Big Blunt"), // Two handed
-	// blunt
-	CROSSBOW(16, "Crossbow");
+	SWORD(2, "Sword"),
+	BLUNT(3, "Blunt"),
+	DAGGER(4, "Dagger"),
+	BOW(5, "Bow"),
+	POLE(6, "Pole"),
+	ETC(7, "Etc"),
+	FIST(8, "Fist"),
+	DUAL(9, "Dual Sword"),
+	DUALFIST(10, "Dual Fist"),
+	BIGSWORD(11, "Big Sword"), // Two Handed Swords
+	PET(12, "Pet"),
+	ROD(13, "Rod"),
+	BIGBLUNT(14, "Big Blunt"); // Two handed blunt
 	private final int _id;
 	private final String _name;
 
