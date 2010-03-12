fbb0c8b6f732677ee9c78b63257ab5a721d3c74a
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-03-12 13:22:38 +0000

    '''CORE SUPPORT:''' Completed Quest Engine of L2j Teon. No more errors in GS now! Fixed Silence Move. Mobs Balanced: Fight, Mage, Priest, Archer and Healer. Fixed Mobs: SS and BSS. L2j Teon system of attacker players separated in: Mobs, Bosses, Summons and Players.
    L2j Teon Pack with more performance now!
    
    Implementation '''PART - 1.'''

--- L2JTeon/java/net/sf/l2j/gameserver/datatables/NpcTable.java
+++ L2JTeon/java/net/sf/l2j/gameserver/datatables/NpcTable.java
@@ -73,7 +73,7 @@ public class NpcTable
 				statement = con.prepareStatement("SELECT "
 						+ L2DatabaseFactory.getInstance().safetyString(
 								new String[] { "id", "idTemplate", "name", "serverSideName", "title", "serverSideTitle", "class", "collision_radius", "collision_height", "level", "sex", "type", "attackrange", "hp", "mp", "hpreg", "mpreg", "str", "con", "dex", "int", "wit", "men", "exp", "sp", "patk", "pdef", "matk", "mdef", "atkspd", "aggro", "matkspd", "rhand", "lhand", "armor", "walkspd",
-										"runspd", "faction_id", "faction_range", "isUndead", "absorb_level", "absorb_type" }) + " FROM npc");
+										"runspd", "faction_id", "faction_range", "isUndead", "absorb_level", "absorb_type", "ss", "bss", "ss_rate", "AI" }) + " FROM npc");
 				ResultSet npcdata = statement.executeQuery();
 				fillNpcTable(npcdata);
 				npcdata.close();
@@ -93,7 +93,7 @@ public class NpcTable
 					statement = con.prepareStatement("SELECT "
 							+ L2DatabaseFactory.getInstance().safetyString(
 									new String[] { "id", "idTemplate", "name", "serverSideName", "title", "serverSideTitle", "class", "collision_radius", "collision_height", "level", "sex", "type", "attackrange", "hp", "mp", "hpreg", "mpreg", "str", "con", "dex", "int", "wit", "men", "exp", "sp", "patk", "pdef", "matk", "mdef", "atkspd", "aggro", "matkspd", "rhand", "lhand", "armor", "walkspd",
-											"runspd", "faction_id", "faction_range", "isUndead", "absorb_level", "absorb_type" }) + " FROM custom_npc");
+											"runspd", "faction_id", "faction_range", "isUndead", "absorb_level", "absorb_type", "ss", "bss", "ss_rate", "AI" }) + " FROM custom_npc");
 					ResultSet npcdata = statement.executeQuery();
 					int npc_count = _npcs.size();
 					fillNpcTable(npcdata);
@@ -331,6 +331,10 @@ public class NpcTable
 			npcDat.set("isUndead", NpcData.getString("isUndead"));
 			npcDat.set("absorb_level", NpcData.getString("absorb_level"));
 			npcDat.set("absorb_type", NpcData.getString("absorb_type"));
+            npcDat.set("ss", NpcData.getInt("ss")); 
+            npcDat.set("bss", NpcData.getInt("bss")); 
+            npcDat.set("ssRate", NpcData.getInt("ss_rate")); 
+            npcDat.set("AI", NpcData.getString("AI")); 
 			L2NpcTemplate template = new L2NpcTemplate(npcDat);
 			template.addVulnerability(Stats.BOW_WPN_VULN, 1);
 			template.addVulnerability(Stats.BLUNT_WPN_VULN, 1);
@@ -371,7 +375,7 @@ public class NpcTable
 			PreparedStatement st = con.prepareStatement("SELECT "
 					+ L2DatabaseFactory.getInstance().safetyString(
 							new String[] { "id", "idTemplate", "name", "serverSideName", "title", "serverSideTitle", "class", "collision_radius", "collision_height", "level", "sex", "type", "attackrange", "hp", "mp", "hpreg", "mpreg", "str", "con", "dex", "int", "wit", "men", "exp", "sp", "patk", "pdef", "matk", "mdef", "atkspd", "aggro", "matkspd", "rhand", "lhand", "armor", "walkspd", "runspd",
-									"faction_id", "faction_range", "isUndead", "absorb_level", "absorb_type" }) + " FROM npc WHERE id=?");
+									"faction_id", "faction_range", "isUndead", "absorb_level", "absorb_type", "ss", "bss", "ss_rate", "AI" }) + " FROM npc WHERE id=?");
 			st.setInt(1, id);
 			ResultSet rs = st.executeQuery();
 			fillNpcTable(rs);
--- L2JTeon/java/net/sf/l2j/gameserver/skills/l2skills/L2SkillElemental.java
+++ L2JTeon/java/net/sf/l2j/gameserver/skills/l2skills/L2SkillElemental.java
@@ -20,6 +20,7 @@ import net.sf.l2j.gameserver.model.L2ItemInstance;
 import net.sf.l2j.gameserver.model.L2Object;
 import net.sf.l2j.gameserver.model.L2Skill;
 import net.sf.l2j.gameserver.model.L2Summon;
+import net.sf.l2j.gameserver.model.actor.instance.L2NpcInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.network.SystemMessageId;
 import net.sf.l2j.gameserver.network.serverpackets.SystemMessage;
@@ -90,6 +91,11 @@ public class L2SkillElemental extends L2Skill
 				activeSummon.setChargedSpiritShot(L2ItemInstance.CHARGED_NONE);
 			}
 		}
+        else if (activeChar instanceof L2NpcInstance) 
+        {
+            bss = ((L2NpcInstance)activeChar).isUsingShot(false); 
+            ss = ((L2NpcInstance)activeChar).isUsingShot(true); 
+        }
 		for (L2Object target2 : targets) {
 			L2Character target = (L2Character) target2;
 			if (target.isAlikeDead())
