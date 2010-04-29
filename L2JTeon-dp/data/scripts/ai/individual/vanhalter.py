1bd434fdd13a8cc1a0463c3c9960bba8257063bc
meyknho <meyknho@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-04-29 20:13:53 +0000

    '''DATA:''' Fix Van Halter. Thanks: '''ZeuS''' for contributor and test.

--- L2JTeon-dp/data/scripts/ai/individual/vanhalter.py
+++ L2JTeon-dp/data/scripts/ai/individual/vanhalter.py
@@ -1,25 +1,17 @@
-# SANDMAN
 import sys
 from net.sf.l2j.gameserver.model.quest import State
 from net.sf.l2j.gameserver.model.quest import QuestState
 from net.sf.l2j.gameserver.model.quest.jython import QuestJython as JQuest
 from net.sf.l2j.gameserver.instancemanager import VanHalterManager
 
-#NPC
-VAN_HALTER = 29062
-CAPTAIN    = 22188
-
-#TRIOL'S REVELATIONS
-TRIOLS = [32058,32059,32060,32061,32062,32063,32064,32065,32066]
-
-# Main Quest Code
 class vanhalter(JQuest):
 
   def __init__(self,id,name,descr) : JQuest.__init__(self,id,name,descr)
 
-  def onAttack (self,npc,player,damage,isPet,skill) :
+
+  def onAttack (self,npc,player,damage,isPet) :
     npcId = npc.getNpcId()
-    if npcId == VAN_HALTER :
+    if npcId == 29062 :
       maxHp = npc.getMaxHp()
       curHp = npc.getCurrentHp()
       if (curHp / maxHp) * 100 <= 20 :
@@ -27,23 +19,25 @@ class vanhalter(JQuest):
 
   def onKill (self,npc,player,isPet) :
     npcId = npc.getNpcId()
-    if npcId in TRIOLS :
+    if npcId == 32058 or npcId == 32059 or npcId == 32060 or npcId == 32061 or npcId == 32062 or npcId == 32063 or npcId == 32064 or npcId == 32065 or npcId == 32066 :
       VanHalterManager.getInstance().removeBleeding(npcId)
-      VanHalterManager.getInstance().checkTriolRevelationDestroy() # checkToriolRevelationDestroy()
-    if npcId == CAPTAIN :
+      VanHalterManager.getInstance().checkToriolRevelationDestroy()
+    if npcId == 22188 :
       VanHalterManager.getInstance().checkRoyalGuardCaptainDestroy()
-    if npcId == VAN_HALTER :
+    if npcId == 29062 :
       VanHalterManager.getInstance().enterInterval()
 
-# Quest class and state definition
-QUEST = vanhalter(-1,"vanhalter","ai")
+QUEST       = vanhalter(-1,"vanhalter","ai")
 
-# Quest NPC starter initialization
-# High Priestess van Halter
-QUEST.addAttackId(VAN_HALTER)
-QUEST.addKillId(VAN_HALTER)
-# Andreas' Captain of the Royal Guard
-QUEST.addKillId(CAPTAIN)
-# Triol's Revelation
-for Triol in TRIOLS :
-    QUEST.addKillId(Triol)
+QUEST.addAttackId(29062)
+QUEST.addKillId(29062)
+QUEST.addKillId(22188)
+QUEST.addKillId(32058)
+QUEST.addKillId(32059)
+QUEST.addKillId(32060)
+QUEST.addKillId(32061)
+QUEST.addKillId(32062)
+QUEST.addKillId(32063)
+QUEST.addKillId(32064)
+QUEST.addKillId(32065)
+QUEST.addKillId(32066)
