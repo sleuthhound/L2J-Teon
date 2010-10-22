b587042c76273e6456a116d85158d113f4f32460
Paulinho Souza <Paulinho Souza@dd9044c2-082f-48a8-b45e-8eb42a20550d>
2010-10-22 11:51:30 +0000

    DATA: Spawn Baium Fix.

--- trunk/DATA/data/scripts/ai/individual/Baium.java
+++ trunk/DATA/data/scripts/ai/individual/Baium.java
@@ -134,7 +134,7 @@ public class Baium extends L2AttackableAIScript
             {
                 // the time has already expired while the server was offline.  Delete the saved time and
                 // immediately spawn the stone-baium.  Also the state need not be changed from ASLEEP
-                addSpawn(STONE_BAIUM,116067,17484,10110,41740,false,0);
+                addSpawn(STONE_BAIUM,116006,17402,10104,41740,false,0);
                 GrandBossManager.getInstance().setBossStatus(LIVE_BAIUM,ASLEEP);
             }
         }
@@ -169,7 +169,7 @@ public class Baium extends L2AttackableAIScript
 			},100L);
         }
         else
-            addSpawn(STONE_BAIUM,116067,17484,10110,41740,false,0);
+            addSpawn(STONE_BAIUM,116006,17402,10104,41740,false,0);
 	}
 
 	public String onAdvEvent (String event, L2NpcInstance npc, L2PcInstance player)
@@ -177,7 +177,7 @@ public class Baium extends L2AttackableAIScript
         if (event.equalsIgnoreCase("baium_unlock"))
         {
             GrandBossManager.getInstance().setBossStatus(LIVE_BAIUM,ASLEEP);
-            addSpawn(STONE_BAIUM,116067,17484,10110,41740,false,0);
+            addSpawn(STONE_BAIUM,116006,17402,10104,41740,false,0);
         }
         else if (event.equalsIgnoreCase("skill_range") && npc != null)
         {
@@ -233,7 +233,7 @@ public class Baium extends L2AttackableAIScript
                 if (_LastAttackVsBaiumTime + 1800000 < System.currentTimeMillis())
                 {
                     npc.deleteMe();   // despawn the live-baium
-                    addSpawn(STONE_BAIUM,116067,17484,10110,41740,false,0);  // spawn stone-baium
+                    addSpawn(STONE_BAIUM,116006,17402,10104,41740,false,0);  // spawn stone-baium
                     GrandBossManager.getInstance().setBossStatus(LIVE_BAIUM,ASLEEP);    // mark that Baium is not awake any more
                     _Zone.oustAllPlayers();
                     cancelQuestTimer("baium_despawn", npc, null);
